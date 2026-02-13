ef16de21eea312f151e0f62b41eb125cb822bfb7
hudi <huxiaodilw@capinfo.com.cn>
2026-02-13 17:42:02 +0800

    文档来源

--- quick-start/src/test/java/com/hudi/spingai/quickstart/TestDeepseek.java
+++ quick-start/src/test/java/com/hudi/spingai/quickstart/TestDeepseek.java
@@ -1,6 +1,9 @@
 package com.hudi.spingai.quickstart;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.deepseek.DeepSeekAssistantMessage;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,13 +20,22 @@ public class TestDeepseek {
 
     @Test
     public void testDeepseek(@Autowired DeepSeekChatModel chatModel) {
-        String call = chatModel.call("你好，你是谁");
-        System.out.println("call = " + call);
+        ChatResponse call = chatModel.call(new Prompt("你好，你是谁"));
+        DeepSeekAssistantMessage message = (DeepSeekAssistantMessage) call.getResult().getOutput();
+        System.out.println("tinkMsg = " + message.getReasoningContent());
+        System.out.println("\t");
+        System.out.println("call = " + message.getText());
+
     }
 
     @Test
     public void testDeepseekStream(@Autowired DeepSeekChatModel chatModel) {
-        Flux<String> stream = chatModel.stream("你好，你是谁");
-        stream.toIterable().forEach(System.out::println);
+//        DeepSeekChatOptions options = DeepSeekChatOptions.builder()
+//               .maxTokens().stop().temperature()
+//                .build();
+//        Prompt prompt = new Prompt("你好，你是谁", options);
+        Prompt prompt = new Prompt("你好，你是谁");
+        Flux<ChatResponse> stream = chatModel.stream(prompt);
+        stream.toIterable().forEach(i-> System.out.println(i.getResult().getOutput().getText()));
     }
 }
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://www.yuque.com/geren-t8lyq/ncgl94/nn41bs3n8svmgpbq?singleDoc#
